5892d2536d917e49ad67202bf0acb1c642c16d6e
Eduardo Vicente <eduardomcv42@gmail.com>
2023-02-10 19:44:37 +0000

    add router provider and root route

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Root } from './routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />
+  }
+])
+
 export function App () {
-  return <div>TODO</div>
+  return <RouterProvider router={router} />
 }
