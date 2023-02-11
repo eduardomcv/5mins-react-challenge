40ebd9137fc765c8c943b6bb6c4212021b240200
Eduardo Vicente <eduardomcv42@gmail.com>
2023-02-11 22:33:31 +0000

    add inputRef and renderOption prop

--- src/components/Combobox/Combobox.jsx
+++ src/components/Combobox/Combobox.jsx
@@ -1,24 +1,29 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Close as CloseIcon } from '../../icons/Close'
 import classes from './Combobox.module.css'
 
-export function Combobox ({ options }) {
+export function Combobox ({ onChange, options, renderOption }) {
   const [inputValue, setInputValue] = useState('')
+  const inputRef = useRef()
 
   const isDropdownOpen = options.length > 0 && inputValue.length > 0
 
   function handleInputChange (event) {
-    setInputValue(event.target.value)
+    const { value } = event.target
+    setInputValue(value)
+    onChange(value)
   }
 
   function handleClear () {
     setInputValue('')
+    inputRef.current?.focus()
   }
 
   return (
     <div className={classes.container}>
       <div className={classes.inputWrapper}>
         <input
+          ref={inputRef}
           type='search'
           className={classes.input}
           value={inputValue}
@@ -38,7 +43,7 @@ export function Combobox ({ options }) {
       {isDropdownOpen && (
         <div className={classes.dropdown}>
           <ul>
-            {options.map(option => <li key={option.label}>{option.label}</li>)}
+            {options.map(renderOption)}
           </ul>
         </div>
       )}
