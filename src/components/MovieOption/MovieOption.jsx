a8fdba59226f0c5614c8aa48f87b4a329e8fb7c6
Eduardo Vicente <eduardomcv42@gmail.com>
2023-02-12 19:47:45 +0000

    add link to movie route

--- src/components/MovieOption/MovieOption.jsx
+++ src/components/MovieOption/MovieOption.jsx
@@ -1,21 +1,23 @@
+import { Link } from 'react-router-dom'
 import { getImageURL } from '../../utils/getImageURL'
 import classes from './MovieOption.module.css'
 
-export function MovieOption ({ posterPath, releaseDate, title }) {
+export function MovieOption ({ id, posterPath, releaseDate, title }) {
   const releaseYear = releaseDate ? new Date(releaseDate).getFullYear() : null
 
   return (
-    <div className={classes.container}>
-      {posterPath && (
-        <img
-          className={classes.poster}
-          src={getImageURL(posterPath, 'w200')}
-          alt='movie poster'
-        />
-      )}
-      <span className={classes.title}>
-        {title}{releaseYear ? ` (${releaseYear})` : ''}
-      </span>
-    </div>
+    <Link to={`/movie/${id}`}>
+      <div className={classes.container}>
+
+        {posterPath && (
+          <img
+            className={classes.poster}
+            src={getImageURL(posterPath, 'w200')}
+            alt='movie poster'
+          />
+        )}
+        <span>{title}{releaseYear ? ` (${releaseYear})` : ''}</span>
+      </div>
+    </Link>
   )
 }
