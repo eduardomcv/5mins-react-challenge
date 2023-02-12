4387e4784fda2089b63c7638aa462622741bff7c
Eduardo Vicente <eduardomcv42@gmail.com>
2023-02-12 20:59:01 +0000

    change to v3

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const APIBaseURL = 'https://api.themoviedb.org/4' // using API version 4
+export const APIBaseURL = 'https://api.themoviedb.org/3' // using API version 3
 
 export const APIToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZTA1NTgwNDBlZmUwYTBhN2Q3NjVlZDQ1ZWI5MDU3ZiIsInN1YiI6IjYzZTJjYThkODI4OWEwMDA3ZjE5NmU5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sFKZ_HgCemJhaR2cyZMih6Z6pgNehoN1XVahxAj5XCk'
 
