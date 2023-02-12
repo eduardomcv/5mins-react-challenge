9044e0610d9c95d7560264b0fa6eb6c0f6204f90
Eduardo Vicente <eduardomcv42@gmail.com>
2023-02-12 19:47:38 +0000

    add movie route

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Root } from './routes/root'
+import { Movie } from './routes/movie'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />
+  },
+  {
+    path: '/movie/:id',
+    element: <Movie />
   }
 ])
 
