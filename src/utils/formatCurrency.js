1595a08df32e18be074978fad33cfd8fa799ba07
Eduardo Vicente <eduardomcv42@gmail.com>
2023-02-12 23:29:24 +0000

    add comment

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,3 +1,8 @@
+/**
+ * Using this util with a hardcoded locale.
+ * For a more robust solution to support multiple languages/locales, should use a library like react-intl.
+ * But I think this is good enough for the exercice.
+ */
 export function formatCurrency (number) {
   const formatter = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'USD' })
   return formatter.format(number)
